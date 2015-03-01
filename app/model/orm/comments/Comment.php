97859b7860ba49bc86bf28db2c521bcbd2f05699
Jan Tojnar <jtojnar@gmail.com>
2015-03-02 00:48:29 +0100

    reading null field with default modifier will return default value

--- app/model/orm/comments/Comment.php
+++ app/model/orm/comments/Comment.php
@@ -12,7 +12,7 @@ use Nextras\Orm\Entity\Entity;
  * @property string|NULL $email
  * @property string $content
  * @property DateTime $createdAt
- * @property DateTime|NULL $deletedAt
+ * @property DateTime|NULL $deletedAt {default now}
  * @property Post $post {m:1 PostsRepository $allComments}
  */
 class Comment extends Entity
